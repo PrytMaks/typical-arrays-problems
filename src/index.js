9a8085dd8a7beced6aead8d5cfa452cbb5323eff
PrytMaks <prytkovmaks@gmail.com>
2021-02-23 13:21:53 +0500

    add: solutions for min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ( array == undefined || array.length == 0 ) return 0;
+
+    array.sort((elem1, elem2) => elem1 - elem2);
+    return array[0];     
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ( array == undefined || array.length == 0 ) return 0;
+
+    array.sort( (a, b) => b - a );
+    return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ( array == undefined || array.length == 0 ) return 0;
+
+    let sumValuesArr = 0;
+    for(let i of array) {
+      sumValuesArr += i;
+    }
+
+    let averageValueArr = sumValuesArr / array.length
+    return averageValueArr;
 }
